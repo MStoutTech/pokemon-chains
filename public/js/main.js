8739baef0b6a8833c0a73e43ae7e1257a6e6f6e7
Molly <91211704+MStoutTech@users.noreply.github.com>
2025-09-09 10:02:38 -0700

    implemented lives and top score tracker

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,16 @@
 
 
-let pokemonGuess = Math.floor(Math.random()*1025)
-let pokemonName = ""
-let chainCount = 1
+let pokemonGuess = Math.floor(Math.random()*1025);
+let pokemonName = "";
+let chainCount = 1;
 let pokemonChain = [];
+let topScore = 0;
+let lives = 3;
+
+if (localStorage.getItem("topScore")){
+  topScore = localStorage.getItem("topScore");
+  document.querySelector(".top-score").innerText = topScore;
+}
 
 let url = 'https://pokeapi.co/api/v2/pokemon/'+ pokemonGuess +'/';
 
@@ -23,8 +30,22 @@ let url = 'https://pokeapi.co/api/v2/pokemon/'+ pokemonGuess +'/';
           console.log(`error ${err}`)
       });
 
+function loseHeart(){
+  lives -= 1;
+  if (lives === 2){
+    document.querySelector('#life-1').innerText = "♡"
+  } else if (lives === 1){
+    document.querySelector('#life-2').innerText = "♡"
+  } else {
+    document.querySelector('#life-3').innerText = "♡"
+    document.querySelector('.error-message').innerText += "- GAME OVER"
+    document.querySelector('.active-game-input').classList.add('hidden')
+    document.querySelector('.active-game-button').classList.add('hidden')
+    document.querySelector('#new-game').classList.toggle('hidden')
+  }
+}
 
-document.querySelector('button').addEventListener('click', addToChain)
+document.querySelector('#chain-button').addEventListener('click', addToChain)
 
 function addToChain(){
   pokemonGuess = document.querySelector('input').value.toLowerCase()
@@ -32,6 +53,7 @@ function addToChain(){
   //check for matching letter in chain
   if (pokemonName[pokemonName.length -1] !== pokemonGuess[0]){
     document.querySelector('.error-message').innerText = "Pokemon name does not start with the correct letter!";
+    loseHeart();
   } else {
     document.querySelector('.error-message').innerText = "";
   
@@ -39,6 +61,7 @@ function addToChain(){
   //check for duplicates
   if (pokemonChain.indexOf(pokemonGuess) >= 0){
     document.querySelector('.error-message').innerText = "This pokemon has already been chained!";
+    loseHeart();
   } else {
 
     url = 'https://pokeapi.co/api/v2/pokemon/'+ pokemonGuess +'/';
@@ -47,14 +70,22 @@ function addToChain(){
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        document.querySelector('img').src = data.sprites.front_default;
-        document.querySelector('h3').innerText += "-" + data.name;
+        document.querySelector('#chain-head-img').src = data.sprites.front_default;
+        document.querySelector('#chain-head-name').innerText = data.name;
         pokemonName = data.name;
         pokemonChain.push(pokemonName);
-        document.querySelector('h2').innerText = `Enter the name of a pokemon starting with the last letter of this pokemon: ${pokemonName}`
+        
+        //score keeping
         chainCount += 1;
         document.querySelector('.active-chain').innerText = `${chainCount}`
+        if (chainCount > topScore){
+          topScore = chainCount;
+          localStorage.setItem("topScore", topScore)
+          document.querySelector(".top-score").innerText = topScore;
+        }
 
+
+        //card chain images
         let li = document.createElement('li');
         let div = document.createElement('div');
         let img = document.createElement('img');
@@ -72,6 +103,7 @@ function addToChain(){
       .catch(err => {
           console.log(`error ${err}`)
           document.querySelector('.error-message').innerText = "Pokemon does not exist! Check your spelling";
+          loseHeart();
       });
   }
 }
